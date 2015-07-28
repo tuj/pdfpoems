490b242db6f110b67903876309c35372d8a21de7
tuj <tujensen@gmail.com>
2015-07-28 14:18:43 +0200

    Added basic files

---
+++ js/app.js
@@ -0,0 +1,7 @@
+var app = angular.module('pdfPoems', []);
+
+app.controller('MainController', ['$scope', 
+  function($scope) {
+
+  }]
+); 
---
+++ process.php
@@ -0,0 +1,9 @@
+<?php
+
+// Get parameters from POST.
+
+// Generate pdf.
+
+// Send mail to receiver.
+
+// Send succes result back to the frontend.
