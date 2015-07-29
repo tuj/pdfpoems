c0c56c3d911f21bf878bc0ecb59d639b9ac6206e
tuj <tujensen@gmail.com>
2015-07-29 14:18:10 +0200

    Added angular. Started main page. Changed to utf-8 encoding.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,42 @@
-var app = angular.module('pdfPoems', []);
+﻿function validateEmail(email) {
+  var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+  return re.test(email);
+}
+
+var app = angular.module('poemApp', []);
+
+app.directive('ngEnter', function() {
+  return function(scope, element, attrs) {
+    element.bind("keydown keypress", function(event) {
+      if (event.which === 13) {
+        scope.$apply(function() {
+          scope.$eval(attrs.ngEnter, {'event': event});
+        });
+
+        event.preventDefault();
+      }
+    });
+  };
+});
 
 app.controller('MainController', ['$scope', 
   function($scope) {
-
-  }]
-); 
+    $scope.email = '';
+    $scope.text = '';
+	$scope.disableSubmit = false;
+	
+	function addText(text) {
+	  $scope.text = text + "\r\n" + $scope.text;
+	}
+	
+	$scope.submit = function submit() {
+	  if (!validateEmail($scope.email)) {
+	    addText($scope.email + " is not a valid email. Try again...");
+	  }
+	  else {
+	    addText("Processing...");
+		$scope.disableSubmit = true;
+	  }
+	}
+  }
+]); 
--- process.php
+++ process.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 
 // Get parameters from POST.
 
